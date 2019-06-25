46e005e6c3c08e693dd2ee8affa8b59196db5958
BManTR <xerqan@hotmail.com>
2019-06-25 16:41:51 +0300

    Convert to page action

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.runtime.onInstalled.addListener(function() {
+    console.log("background installed");
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { urlEquals : 'https://stars.bilkent.edu.tr/airs/index.php?do=advs'}
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+  });
